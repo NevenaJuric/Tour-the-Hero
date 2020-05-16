ef05e0334fa94f0e6b09243ad7b45ecc5e7db56c
NevenaJuric <nevenajuric96@gmail.com>
2020-05-16 11:00:24 +0200

    change all aplication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSpecialityComponent } from './hero-speciality/hero-speciality.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'detail/:id', component: HeroDetailComponent},
+  {path: 'speciality/:id', component: HeroSpecialityComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,46 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSpecialityComponent } from './hero-speciality/hero-speciality.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessagesSpecComponent } from './messages-spec/messages-spec.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+// imitira server
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HeroesComponent,
+      HeroDetailComponent,
+      HeroSpecialityComponent,
+      MessagesComponent,
+      MessagesSpecComponent,
+      DashboardComponent,
+      HeroSearchComponent,
+      CrisisListComponent,
+      HeroListComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      FormsModule,
+      HttpClientModule,
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService,
+      {dataEncapsulation: false}
+      )
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
